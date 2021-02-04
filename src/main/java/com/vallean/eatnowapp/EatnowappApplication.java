12ccc6ff34727bc5f990c4be5b6d076b363ee753
ValdirLeanderson <valdirleanderson@yahoo.com.br>
2021-02-03 21:45:17 -0300

    Inicio do projeto

---
+++ src/main/java/com/vallean/eatnowapp/EatnowappApplication.java
@@ -0,0 +1,13 @@
+package com.vallean.eatnowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatnowappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatnowappApplication.class, args);
+	}
+
+}
